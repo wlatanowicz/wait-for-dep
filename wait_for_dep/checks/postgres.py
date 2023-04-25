c71cd2638b46867af0c507125f7754a973433413
Wiktor Latanowicz <wiktor@latanowicz.com>
2023-04-25 08:29:52 +0200

    Fix postgres check with psycopg3

--- wait_for_dep/checks/postgres.py
+++ wait_for_dep/checks/postgres.py
@@ -1,12 +1,14 @@
-import psycopg2
-
+try:
+    import psycopg2 as psycopg
+except ImportError:
+    import psycopg
 
 def check(url):
     if url.startswith("psql"):
         url = "postgres" + url[len("psql") :]
 
     try:
-        conn = psycopg2.connect(url)
+        conn = psycopg.connect(url)
         cur = conn.cursor()
         cur.execute("SELECT NOW();")
         return True
