5e7d5f36ae687faa8f0b56bcbcc0050c344f3ba3
Wiktor <wiktor@latanowicz.com>
2025-03-18 13:50:31 +0100

    Add support for Redis over SSL

--- wait_for_dep/checks/tcp.py
+++ wait_for_dep/checks/tcp.py
@@ -5,6 +5,9 @@ from urllib.parse import urlparse
 def check(url):
     parsed_url = urlparse(url)
     try:
+        if not parsed_url.port:
+            return False
+
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((parsed_url.hostname, parsed_url.port))
         return True
